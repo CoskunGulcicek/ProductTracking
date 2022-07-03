7254b657c16e005ce07394021e925e3e9460f919
Coskun GULCICEK <coskun.gulcicek@bilgibirikim.com>
2022-07-03 21:46:05 +0300

    değişiklik öncesi son push

--- ProductTracking/wwwroot/js/home.js
+++ ProductTracking/wwwroot/js/home.js
@@ -3,9 +3,74 @@
 });
 
 
-function sendCustomer(data) {
-    alert(data);
+function sendCustomer(customerId) {
+    document.getElementById('modalCustomerNameTitle').innerHTML = $("#hiddenCustomerName_" + customerId).val();
+    $('#modalCustomerId').val(customerId);
 };
+
+
+$('#kisiyeUrunEkle').click(function () {
+
+    var eklenenCustomer = $('#modalCustomerId').val();
+    var products = [];
+    $("input:checkbox[name=products]:checked").each(function () {
+        var val = $(this).val();
+        products.push({
+            name: val,
+            quantity: 0,
+            type: 'Ks'
+        });
+    });
+
+    customerAddBasket(eklenenCustomer, products);
+
+    $('#myModal').find("input:checked").prop('checked', false);
+    $('[data-dismiss="modal"]').trigger('click');
+});
+
+function customerAddBasket(cusID, products) {
+
+
+    var customer = data.customers.filter(function (customer) {
+        return customer.id == cusID;
+    })[0];
+    var sendProds = [];
+    products.forEach(function (addProd) {
+        var hasProducst = false;
+        (customer.basket || []).forEach(function (product) {
+            hasProducst = !hasProducst && (addProd.name == product.name && addProd.type == product.type);
+            if (hasProducst) {
+                addProd.quantity = parseInt(addProd.quantity) + parseInt(product.quantity);
+            }
+        });
+
+        sendProds.push(addProd);
+    });
+
+    customer.basket = sendProds;
+
+    render();
+}
+
+$(document).on('change', '.inpt-quantity, .inpt-type', function (e) {
+    var product = $(e.target).parents('.product');
+    
+    var cusID = product.parent().data('cusid');
+    var name = product.find('label').data('name');
+    var type = product.find('select option:checked').val();
+    var quantity = product.find('[name=quantityofproduct]').val();
+
+    var customer = data.customers.filter(function (customer) {
+        return customer.id == cusID;
+    })[0];
+
+    (customer.basket || []).forEach(function (product) {
+        if (name == product.name && type == product.type) {
+            product.quantity = parseInt(quantity);
+        }
+    });
+    // render();
+});
 
 $('#btnAddDealCalculate').click(function () {
     function sendCalculationData(data) {
@@ -20,7 +85,7 @@ $('#btnAddDealCalculate').click(function () {
                     //data[i].name
                     //data[i].type
                     //data[i].quantity
-                    
+
                     var alanlar = "<div class='col-sm-2  border-top border-bottom border-left'><strong>" + data[i].name + "</strong></div> <div class='col-sm-1 border-top border-bottom'><strong>" + data[i].type + "</strong></div> <div class='col-sm-1 border-top border-bottom border-right'><strong>" + data[i].quantity + "</strong></div>";
                     $("#totalDiv").append(alanlar);
                 }
@@ -42,15 +107,29 @@ $('#btnAddDealCalculate').click(function () {
     var calculationData = [];
     var pCount = $('#productCount').val();
     var cCount = $('#customerCount').val();
+
     if (pCount > 0 && cCount > 0) {
-        for (let i = 1; i <= cCount; i++) {
-            for (let j = 1; j <= pCount; j++) {
-                var Name = document.getElementById('name_' + i + '_' + j).textContent;
-                var Type = $('#type_' + i + '_' + j).val();
-                var Quantity = $('#quantity_' + i + '_' + j).val();
-                calculationData.push({ Name, Type, Quantity });
-            }
-        }
+
+        //data.customers.forEach(function (customer) {
+        //    (customer.basket || []).forEach(function (item) {
+        //        calculationData.push(item);
+        //    });
+        //});
+
+        $('.product').each(function () {
+            var product = $(this);
+
+            var name = product.find('label').data('name');
+            var type = product.find('select option:checked').val();
+            var quantity = product.find('[name=quantityofproduct]').val();
+
+            calculationData.push({
+                name: name,
+                type: type,
+                quantity: quantity
+            });
+        });
+
     } else {
         console.log("ürün yok");
     }
@@ -58,3 +137,18 @@ $('#btnAddDealCalculate').click(function () {
     sendCalculationData(calculationData);
 
 });
+
+var templateHTML = $('#data-template').html();
+var container = $('#content-pane');
+var template;
+$(function () {
+    template = Handlebars.compile(templateHTML);
+    render();
+});
+
+function render() {
+    container.empty();
+    container.append(template(data));
+}
+
+
--- Tracking.DataAccess/Concrete/EntityFrameworkCore/Context/TrackingContext.cs
+++ Tracking.DataAccess/Concrete/EntityFrameworkCore/Context/TrackingContext.cs
@@ -13,7 +13,8 @@ namespace Tracking.DataAccess.Concrete.EntityFrameworkCore.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ProductTrackingDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ProductTrackingDb;Trusted_Connection=True;MultipleActiveResultSets=true");//home
+            optionsBuilder.UseSqlServer(@"Server=MSCOSKUNG;Database=ProductTrackingDb;Trusted_Connection=True;MultipleActiveResultSets=true");//w
             //optionsBuilder.UseSqlServer(@"Server=77.245.159.10;Database=coskung1_;Uid=cg;Pwd=59U8eF3zc83M6Cj3;");
             //optionsBuilder.UseSqlServer(@"Server=.\MSSQLSERVER2019;Database=coskung1_;Uid=cg;Pwd=59U8eF3zc83M6Cj3;");
         }
